b552d5d779d18c3bab83df6a65ee99134382c64b
Dane Ross <quintanodr@gmail.com>
2025-11-15 02:59:49 +0800

    publish bebols world

--- assets/js/project-gallery.js
+++ assets/js/project-gallery.js
@@ -56,9 +56,14 @@
       '7.jpg',
     ],
     'bebol': [
-      '1.png',
-      '2.png',
-      '3.png'
+      '1.jpg',
+      '2.jpg',
+      '3.jpg',
+      '4.jpg',
+      '5.jpg',
+      '6.jpg',
+      '7.jpg',
+      '8.jpg'
     ]
     // Add more projects here as needed
   };
