62fab8afde0abdc8f09b8f18eeaeb432c7de006e
Dane Ross <quintanodr@gmail.com>
2025-11-15 01:14:01 +0800

    add sari project imgs

--- assets/js/project-gallery.js
+++ assets/js/project-gallery.js
@@ -47,9 +47,13 @@
       '3.png'
     ],
     'sari': [
-      '1.png',
-      '2.png',
-      '3.png'
+      '1.jpg',
+      '2.jpg',
+      '3.jpg',
+      '4.jpg',
+      '5.jpg',
+      '6.jpg',
+      '7.jpg',
     ],
     'bebol': [
       '1.png',
